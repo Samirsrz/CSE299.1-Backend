2242a6c1c3f8bc59753cc314936661772cb55ca4
Samirsrz <shahilahamed2001@gmail.com>
2025-03-01 17:05:18 +0600

    changed port in backend

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 require('dotenv').config();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 
 
